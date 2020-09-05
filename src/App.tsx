15adaa55b69acb565cbb9e45fef761f5bd548edc
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-09-05 01:42:50 -0300

    removes explicit firebase credentials

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/__/firebase/init.json').then(async response => {
+      firebase.initializeApp(await response.json());
+      firebase.analytics();
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <>
-      <Routes />
+      {!loading && <Routes />}
       <GlobalStyles />
     </>
   );
