15e8194d7117d335f72b66bd835618ffeeec6810
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-09-06 13:45:49 -0300

    setup local firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,30 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('/__/firebase/init.json').then(async response => {
-      firebase.initializeApp(await response.json());
+    (async () => {
+      const response = await fetch('/__/firebase/init.json');
+
+      let firebaseConfig = {};
+
+      if (process.env.NODE_ENV === 'development') {
+        firebaseConfig = {
+          apiKey: process.env.REACT_APP_API_KEY,
+          authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+          databaseURL: process.env.REACT_APP_DATABASE_URL,
+          projectId: process.env.REACT_APP_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+          appId: process.env.REACT_APP_APP_ID,
+          measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+        };
+      } else {
+        firebaseConfig = await response.json();
+      }
+
+      firebase.initializeApp(firebaseConfig);
       firebase.analytics();
       setLoading(false);
-    });
+    })();
   }, []);
 
   return (
