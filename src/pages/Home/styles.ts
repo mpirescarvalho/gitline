5013bc0955f613bcf7aa8bb461014cc48f3d82fd
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-09-06 14:27:58 -0300

    add search page responsivity

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -33,21 +33,23 @@ export const AutoCompleteItem = styled.div`
   border-top: 1px solid var(--border);
   color: var(--text-secondary);
 
-  &.active {
-    background: var(--secondary);
-    color: var(--text-in-secondary);
+  @media (min-width: 601px) {
+    &.active {
+      background: var(--secondary);
+      color: var(--text-in-secondary);
 
-    &::after {
-      content: 'Enter';
-      width: 43px;
-      height: 22px;
-      color: var(--text-secondary);
-      font-family: 'Russo One', sans-serif;
-      font-size: 10px;
-      background: var(--box-in-secondary);
-      line-height: 22px;
-      text-align: center;
-      border-radius: 8px;
+      &::after {
+        content: 'Enter';
+        width: 43px;
+        height: 22px;
+        color: var(--text-secondary);
+        font-family: 'Russo One', sans-serif;
+        font-size: 10px;
+        background: var(--box-in-secondary);
+        line-height: 22px;
+        text-align: center;
+        border-radius: 8px;
+      }
     }
   }
 `;
