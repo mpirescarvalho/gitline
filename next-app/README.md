961d53693a47719467486811556e1210a1ee6a58
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-09-28 21:28:54 -0300

    add static files

---
+++ next-app/README.md
@@ -0,0 +1,50 @@
+# Gitline
+
+> Your github repositories history beatifuly structured
+
+<p align="start">
+	  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/mpirescarvalho/gitline?style=flat">
+	  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/mpirescarvalho/gitline">
+	  <img alt="GitHub stars" src="https://img.shields.io/github/stars/mpirescarvalho/gitline?style=social">
+	  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/mpirescarvalho/gitline">
+	  <a href="https://gitline.web.app">
+			<img alt="Website" src="https://img.shields.io/website?url=https%3A%2F%2Fgitline.web.app">
+    </a>
+</p>
+
+View your github repositories timeline, beatifuly organized and sorted by creation date. Filter them by the main language and share with anyone you want.
+
+## Live
+
+You can find the application live here:
+https://gitline.web.app
+
+<h1 align="start">
+  <img alt="Gitline" title="#Gitline" src="./assets/gitline.gif" />
+</h1>
+
+## Contributing
+
+This repository is currently under development. If you want to contribute please fork the repository and get your hands dirty, and make the changes as you'd like and submit the Pull request.
+
+## Building
+
+You'll need [Node.js](https://nodejs.org) installed on your computer in order to build this app.
+
+```bash
+$ git clone https://github.com/mpirescarvalho/gitline.git
+$ cd gitline
+$ npm install
+$ npm run start
+```
+
+Runs the app in the development mode.<br/>
+
+## Contact
+
+<a href="https://github.com/mpirescarvalho">
+  <img alt="made by Marcelo Carvalho" src="https://img.shields.io/badge/made%20by-Marcelo Carvalho-%237519C1">
+</a>
+<a href="mailto:mpirescarvalho17@gmail.com">
+  <img alt="made by Marcelo Carvalho" src="https://img.shields.io/badge/-mpirescarvalho17@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:mpirescarvalho17@gmail.com" />
+</a>
--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -22,8 +22,56 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
 
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gitline.web.app/" />
+      <meta
+        property="og:title"
+        content="Gitline · Your github repositories history beatifuly structured"
+      />
+      <meta
+        property="og:description"
+        content="Gitline is the right place to view your github repositories timeline beatifuly organized and sorted by creation date. Filter them by the main language and share with anyone you want."
+      />
+      <meta
+        property="og:image"
+        content="https://gitline.web.app/social-img.jpg"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://gitline.web.app/" />
+      <meta
+        property="twitter:title"
+        content="Gitline · Your github repositories history beatifuly structured"
+      />
+      <meta
+        property="twitter:description"
+        content="Gitline is the right place to view your github repositories timeline beatifuly organized and sorted by creation date. Filter them by the main language and share with anyone you want."
+      />
+      <meta
+        property="twitter:image"
+        content="https://gitline.web.app/social-img.jpg"
+      />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
     <Component {...pageProps} />
     <GlobalStyles />
   </ThemeProvider>
